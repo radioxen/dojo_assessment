f017068ab48f443126eaaf0ca68013876d3ea00e
radioxen <hessam.k.rad@gmail.com>
2023-02-14 15:00:34 -0500

    added frontend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, BackgroundTasks, Response
+from fastapi.responses import StreamingResponse
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse, HTMLResponse
 import pandas as pd
 from io import BytesIO
 from backend.directions import direction_main
@@ -15,6 +16,7 @@ origins = [
     "localhost:3000"
 ]
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -23,37 +25,21 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get("/")
-async def main():
-    """
-    receives an input csv file, containing home and target address:
-        - 2 columns, headers are mendatory, first column is home address and second column should be the target.
-        - there is no restriction for both addresses as long as they are valid.
-        - overseas travel is not implemented.
-    :return: a chart, illustrating travel time of driving versus public transit for all trips.
-    """
-    content = """
-        <body>
-        <form action="/chart" enctype="multipart/form-data" method="post">
-        <input name="file" type="file" single>
-        <input type="submit">
-        </body>
-        """
-    return HTMLResponse(content=content)
+
+# @app.get("/")
+# async def root():
+#     csv_file = "test/sample_addresses.csv"
+#     df = pd.read_csv(csv_file)
+#     values = df.values.tolist()
+#     response = await direction_main(address_pairs=values)
+#     return JSONResponse(content=response)
 
 
-@app.post("/chart")
-async def get_chart(file: UploadFile):
-    """
+@app.get('/')
+async def get_chart():
 
-    :param file: csv file for iinpu with the iinstructed format : headers, 2 columns with 1st home and 2nd target addresses,
-    :return: a bar plot with units and legend, demonstrating travel time for 2 modes of transit.
-    """
-    contents = file.file.read()
-    buffer = BytesIO(contents)
-    df = pd.read_csv(buffer)
-    buffer.close()
-    file.file.close()
+    csv_file = "test/sample_addresses.csv"
+    df = pd.read_csv(csv_file)
     df.reset_index(inplace=True)
     values = df.values.tolist()
     response = await direction_main(address_pairs=values)
